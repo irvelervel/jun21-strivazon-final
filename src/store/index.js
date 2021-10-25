6a945e4d6790a53f7a25111d2d0102dbf2dbcb7c
Stefano Casasola <ste.casasola@gmail.com>
2021-10-25 12:26:54 +0200

    Finishes the lecture

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,7 @@ import bookReducer from '../reducers/book'
 import thunk from 'redux-thunk'
 import { persistStore, persistReducer } from 'redux-persist'
 import localStorage from 'redux-persist/lib/storage'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -32,6 +33,16 @@ export const initialState = {
 const persistConfig = {
   key: 'root',
   storage: localStorage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY,
+      // the secret key will be used for encrypt/decrypt the stringified version of your redux
+      // store saved in the engine of choice
+      onError: (error) => {
+        console.log(error)
+      },
+    }),
+  ],
 }
 
 const bigReducer = combineReducers({
